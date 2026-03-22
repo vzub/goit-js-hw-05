305930aeaf7bf8f9ed0ea90252d5bd7ae5d012c2
Vladyslav <vzub210201@gmail.com>
2026-03-22 16:17:26 +0200

    upd

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = (users) => {
-    return users.toSorted((a, b) => b.friends - a.friends);
+    return users.toSorted((a, b) => b.friends.length - a.friends.length);
 }
 console.log(
   sortByDescendingFriendCount([
